b33005e4a161f3cf939fbac681f52aab0a9ff0a2
KoshmareG <Koshmareg@yandex.ru>
2022-03-09 19:44:24 +0700

    Create product migration

--- public/app.js
+++ public/app.js
@@ -1,7 +1,10 @@
-var x = 1;
-
 function something()
 {
+    var x = window.localStorage.getItem('b');
+
+    x = x * 1 + 1;
+
+    window.localStorage.setItem('b', x);
+
     alert(x);
-    x = x + 1;
 }
