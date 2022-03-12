1711037d384e966006555c5ebfa4c411062deb22
KoshmareG <Koshmareg@yandex.ru>
2022-03-12 22:56:53 +0700

    Add enumerate function for cart

--- public/app.js
+++ public/app.js
@@ -1,7 +1,21 @@
 function add_to_cart(id)
 {
-    var key = 'product' + id;
+    var key = 'product_' + id;
     var x = window.localStorage.getItem(key);
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
 }
+
+function cart_total()
+{
+    var total = 0;
+    var cart_array = [];
+    for(var i in localStorage){
+        if(localStorage.hasOwnProperty(i)){
+            cart_array.push(localStorage[i]);
+        }
+    }
+    cart_array.forEach(Element => total = total * 1 + Element * 1);
+}
+
+cart_total();
