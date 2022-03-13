023f853eda547373027d4ea3991cf5cf9f3de02a
KoshmareG <Koshmareg@yandex.ru>
2022-03-13 16:13:19 +0700

    Add cart_orders

--- public/app.js
+++ public/app.js
@@ -22,4 +22,20 @@ function cart_total()
     return total;
 }
 
+function cart_orders()
+{
+    var orders = '';
+    for(var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i);
+        var value = window.localStorage.getItem(key);
+
+        if(key.indexOf('product_') == 0)
+        {
+            orders = orders + key + '=' + value + ',';
+        }
+    }
+    return orders;
+}
+
 cart_total();
